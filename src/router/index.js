c92ab4a2ade55e7667d61800c193daec5f75b9e7
SepidehBBVDomke <s.ghanbary@bbv-domke.de>
2024-10-15 11:42:47 +0200

    router ist konfiguriert

--- src/router/index.js
+++ src/router/index.js
@@ -1,13 +1,26 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import AboutView from '../views/AboutView.vue'
+import ProductListView from '../views/ProductListView.vue'
+import NotFoundView from '../views/NotFoundView.vue'
 
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      // path: '/',
-      // name: 'home',
-      //component: HomeView
+      path: '/about',
+      name: 'about',
+      component: AboutView
+    },
+    {
+      path: '/',
+      name: 'products',
+      component: ProductListView
+    },
+    {
+      path: '/:pathMatch(.*)',
+      name: '404',
+      component: NotFoundView
     },
     {
       //path: '/about',
---
+++ src/views/ProductListView.vue
@@ -0,0 +1,3 @@
+<template>
+    <h2>Liste</h2>
+</template>
