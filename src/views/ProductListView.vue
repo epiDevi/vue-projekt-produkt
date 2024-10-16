e31339c3c660b9346de336e348a5208771cca9a4
SepidehBBVDomke <s.ghanbary@bbv-domke.de>
2024-10-16 08:22:47 +0200

    liste

--- src/views/ProductListView.vue
+++ src/views/ProductListView.vue
@@ -1,3 +1,36 @@
 <template>
     <h2>Liste</h2>
+    <div class="product-container">
+        <div class="card" v-for="product in products" :key="product.id">
+            <img :src="product.imageName" :alt="product.name" class="card__img">
+            <div class="card__details">
+                <h3 class="card__name">
+                    {{ product.name }}
+
+                </h3>
+                <p class="card__price">
+                    {{ product.price }}
+                </p>
+            </div>
+
+        </div>
+    </div>
 </template>
+<script setup>
+import { products } from '../data.js'
+</script>
+<style>
+.product-container{
+    display: grid;
+    grid-template-columns: repeat(3,1fr ) ;
+    gap: 2rem;
+    justify-items: center;
+}
+.card__details{
+    background-color: white;
+    padding: 2rem;
+}
+.card__name{
+    margin-bottom: 1rem;
+}
+</style>
